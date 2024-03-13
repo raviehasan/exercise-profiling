6e3169d208c399c896150362aae2be9188666801
raviehasan <ravie.hasan@ui.ac.id>
2024-03-13 12:24:07 +0700

    [Refactor] - create findAll method for optimizing

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
@@ -1,6 +1,5 @@
 package com.advpro.profiling.tutorial.repository;
 
-import com.advpro.profiling.tutorial.model.Student;
 import com.advpro.profiling.tutorial.model.StudentCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,4 +12,5 @@ import java.util.List;
 @Repository
 public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
     List<StudentCourse> findByStudentId(Long studentId);
+    List<StudentCourse> findAll();
 }
